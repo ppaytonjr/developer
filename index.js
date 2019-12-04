3dd72d5d51bc9566c7bdeb8ba3759e749080bcdb
Payton <p.paytonjr@outlook.com>
2019-12-04 16:32:56 -0600

    updated and included pdf converter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 var pdf = require('html-pdf');
 const axios = require("axios");
 const inquirer = require("inquirer");
+var pdfcrowd = require("pdfcrowd");
+
 
 inquirer.prompt([
     {
@@ -21,11 +23,13 @@ inquirer.prompt([
     const queryUrl = `https://api.github.com/users/${username}`;
     
     axios.get(queryUrl).then(function(res){
-        const { avatar_url, followers, login, name } = res.data;
+        const { avatar_url, followers, repos_url, login, name, following, bio, starred_url, following_url, followers_url  } = res.data;
         const html = `
+        
             <html>
                 <head>
                     <style>
+                    <div id="html-2-pdfwrapper">
                         body {
                             background-color: ${color};
                         }
@@ -34,19 +38,34 @@ inquirer.prompt([
                 <body>
                     <h1>${name} | ${login}</h1>
                     <img width="100" height="100" src="${avatar_url}" />
+                    <br>${bio}</br>
+                    <a href="${repos_url}" target="_blank">Link to Repos</a>
+                    <br><a href="${followers_url}">Followers:${followers}</a></br>
+                    <br><a href="${following_url}" target="_blank">Following:${following}</a></br>
+                    <br><a href="${starred_url}" target="_blank">Starrs</a></br>
                 </body>
             </html>
         `
-        fs.writeFile('test.html', html, function(err) {
+        fs.writeFile('index.html', html, function(err) {
             if (err) throw err;
         });              
     });
 });
-        
+var client = new pdfcrowd.HtmlToPdfClient(
+    "demo", "ce544b6ea52a5621fb9d55f8b542d14d");
 
-// function writeToFile("profile.pdf", data) {
+    var callbacks = pdfcrowd.saveToFile("index.pdf");        
+
+    callbacks.error = function(errMessage, statusCode) {
+        if(statusCode) {
+            console.error("Pdfcrowd Error: " + statusCode + " - " + errMessage);
+        } else {
+            console.error("Pdfcrowd Error: " + errMessage);
+        }
+    };
+
+    client.convertUrl("https://ppaytonjr.github.io/developer", callbacks);
  
-// }
 
 // function init() {
 
---
+++ node_modules/pdfcrowd/README.md
@@ -0,0 +1,35 @@
+# Pdfcrowd API - Node.js client library
+
+The Pdfcrowd API lets you easily convert between HTML, PDF and various image
+formats in your Node.js applications.
+
+To use the API, you need an account on
+[http://pdfcrowd.com](https://pdfcrowd.com). If you don't have one, you
+can sign up [here](https://pdfcrowd.com/pricing/api/).
+
+## Installation
+
+To install via npm:
+
+    $ npm install pdfcrowd
+
+Or clone from GitHub and create a symlink in `~/.node_libraries`:
+
+    $ git clone git@github.com:pdfcrowd/pdfcrowd-nodejs.git
+    $ ln -s /path/to/pdfcrowd-nodejs ~/.node_libraries/pdfcrowd
+
+Dependencies
+
+* http *native module*
+* querystring *native module*
+* fs *native module*
+
+
+## Documentation
+
+* API Home:  <https://pdfcrowd.com/doc/api/>
+* API Reference:  <https://pdfcrowd.com/doc/api/html-to-pdf/nodejs/>
+
+## License
+
+This client library is licensed under the MIT License.
